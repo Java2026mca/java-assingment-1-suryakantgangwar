79829849e32551d7a49fb9108edb0bea611c83ea
suryakantgangwar <suryakantgangwar16@gmail.com>
2026-04-07 15:44:25 +0530

    Suryakant_01

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -6,7 +6,42 @@ public class Main {
         int n = sc.nextInt();
 
         // TODO: Print a hollow diamond pattern of half-height n using '*'
-        //
+        for (int i = 1; i <= n; i++) {
+    // spaces
+    for (int j = 1; j <= n - i; j++) {
+        System.out.print(" ");
+    }
+
+    // stars and spaces inside
+    for (int j = 1; j <= 2 * i - 1; j++) {
+        if (j == 1 || j == 2 * i - 1) {
+            System.out.print("*");
+        } else {
+            System.out.print(" ");
+        }
+    }
+
+    System.out.println();
+}
+
+// Lower half
+for (int i = n - 1; i >= 1; i--) {
+    // spaces
+    for (int j = 1; j <= n - i; j++) {
+        System.out.print(" ");
+    }
+
+    // stars and spaces inside
+    for (int j = 1; j <= 2 * i - 1; j++) {
+        if (j == 1 || j == 2 * i - 1) {
+            System.out.print("*");
+        } else {
+            System.out.print(" ");
+        }
+    }
+
+    System.out.println();
+}
         // Rules:
         //   - Only border cells have '*', inside is spaces
         //   - No trailing spaces on any line
